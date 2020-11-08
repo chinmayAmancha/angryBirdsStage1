d6476e96eb9baf6001536975a06d30dda6796701
chinmayAmancha <72676532+chinmayAmancha@users.noreply.github.com>
2020-11-08 18:54:49 -0500

    Update Log.js

--- Log.js
+++ Log.js
@@ -1,28 +1,29 @@
 class Log {
-    constructor(x, y, height, angle) {
-      var options = {
-          'restitution':0.8,
-          'friction':1.0,
-          'density':1.0
-      }
-      this.body = Bodies.rectangle(x, y, 20, height, options);
-      this.width = 20;
-      this.height = height;
-      Matter.Body.setAngle(this.body, angle);
-      World.add(world, this.body);
+constructor(x,y,height,angle) {
+
+    var options = {
+    'restitution': 1.3,
+    'friction': 1.0,
+    'density': 1.0
     }
-    display(){
-      var pos =this.body.position;
-      var angle = this.body.angle;
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      strokeWeight(4);
-      stroke("green");
-      fill(255);
-      rect(0, 0, this.width, this.height);
-      pop();
+    
+    this.body = Bodies.rectangle(x,y,20,height,options);
+    Matter.Body.setAngle(this.body,angle);
+    this.width = 20;
+    this.height = height;
+    
+    World.add(world,this.body);
+}
+    display() {
+    var angle = this.body.angle;
+    
+    push();
+    rotate(angle);
+    translate(this.body.position.x,this.body.position.y);
+    strokeWeight(3);
+    stroke(black);
+    rectMode(CENTER);
+    rect(x,y,20,height);
+    pop();
     }
-  };
-  
+}
